1159a2576ef05b14a090a8c6e626fabdbb9519d7
Professor <professor@professor.professor.net>
2022-08-09 16:46:00 +0300

    first commit

--- database/db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://moses:<password>@cluster0.90jueup.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
